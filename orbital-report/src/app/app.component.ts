bfb54404f25d1837f262140bb91fe1de55a76b39
foodDinner <specialdelivery@gmail.com>
2019-10-27 23:40:24 -0500

    searching

--- orbital-report/src/app/app.component.ts
+++ orbital-report/src/app/app.component.ts
@@ -8,9 +8,10 @@ import { Satellite } from './satellite';
 })
 export class AppComponent {
   title = 'orbital-report';
-  
+  displayList: Satellite[];
   sourceList: Satellite[] = [];
   constructor() {
+    this.displayList = [];
     this.sourceList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
  
@@ -24,10 +25,21 @@ export class AppComponent {
             let satelliteNew = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
             this.sourceList.push(satelliteNew);
           }
- 
+          this.displayList = this.sourceList.slice(0);
        }.bind(this));
     }.bind(this));
- 
  }
-
+ search(searchTerm: string): void {
+  let matchingSatellites: Satellite[] = [];
+  searchTerm = searchTerm.toLowerCase();
+  for(let i=0; i < this.sourceList.length; i++) {
+     let name = this.sourceList[i].aname.toLowerCase();
+     if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+     }
+  }
+  // assign this.displayList to be the the array of matching satellites
+  // this will cause Angular to re-make the table, but now only containing matches
+  this.displayList = matchingSatellites;
+  }
 }
